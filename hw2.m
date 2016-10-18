a3d7b977b8cd65e997cd27728569dc60004f1396
brocklesby <wsb@MacBook-Pro-3.local>
2016-10-18 09:57:43 +0100

    "small change 2"

--- hw2.m
+++ hw2.m
@@ -5,4 +5,4 @@ fprintf('hello, world\n')
 fprintf('this is stored on Github, bizarrely...\n')
 %new branch to see what happens...
 fprintf('cant see the trees\n')
-fprintf('cant see the trees\n')
+fprintf('cant see the trees\n');
